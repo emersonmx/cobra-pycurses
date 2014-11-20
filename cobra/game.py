687164400639dc9e67866e50a92e6f2451161341
Emerson MX <emersonmx@gmail.com>
2014-11-20 16:29:57 -0300

    Protótipo incial.

--- cobra/game.py
+++ cobra/game.py
@@ -1,7 +1,35 @@
 import sys
 import curses
 
-from cobra.screen import MenuScreen
+from cobra.model import Snake, SnakeListener
+
+
+class SnakeRenderer(SnakeListener):
+
+    def __init__(self):
+        super(SnakeRenderer, self).__init__()
+
+        self._draw_list = []
+        self._dirty_list = []
+
+    def body_updated(self, body):
+        self._draw_list = list(body)
+
+    def head_updated(self, head):
+        self._draw_list.append(head)
+
+    def tail_updated(self, tail):
+        self._dirty_list.append(tail)
+
+    def draw(self, stdscr):
+        for x, y in self._draw_list:
+            stdscr.addch(y, x, '#')
+
+        for x, y in self._dirty_list:
+            stdscr.addch(y, x, ' ')
+
+        self._draw_list = []
+        self._dirty_list = []
 
 
 class Game(object):
@@ -11,39 +39,50 @@ class Game(object):
 
         self.stdscr = None
         self._window_size = []
-        self._screen = None
 
         self._running = True
         self._error_code = 0
 
+        self.snake = None
+        self.snake_renderer = None
+
     @property
     def window_size(self):
         return self._window_size
-
-    @property
-    def screen(self):
-        return self._screen
-
-    @screen.setter
-    def screen(self, screen):
-        if self._screen:
-            self._screen.dispose()
-
-        screen.create()
-        self._screen = screen
 
     def exit(self, error_code=0):
         self._error_code = error_code
         self._running = False
 
     def create(self):
-        self._screen.create()
+        size = 5
+        x, y = self.window_size[1] / 2 - size, self.window_size[0] / 2
+        self.snake = Snake([(x+i, y) for i in xrange(size)])
+        self.snake_renderer = SnakeRenderer()
+        self.snake.listener = self.snake_renderer
 
     def dispose(self):
-        self._screen.dispose()
+        pass
 
     def update(self):
-        self._screen.update()
+        curses.napms(200)
+
+        key = self.stdscr.getch()
+        curses.flushinp()
+
+        if key == curses.KEY_UP:
+            self.snake.direction = Snake.UP
+        elif key == curses.KEY_RIGHT:
+            self.snake.direction = Snake.RIGHT
+        elif key == curses.KEY_DOWN:
+            self.snake.direction = Snake.DOWN
+        elif key == curses.KEY_LEFT:
+            self.snake.direction = Snake.LEFT
+
+        self.snake.update()
+        self.snake_renderer.draw(self.stdscr)
+
+        self.stdscr.refresh()
 
     def _setup(self, stdscr, args):
         curses.resizeterm(24, 80)
@@ -51,11 +90,10 @@ class Game(object):
         curses.use_default_colors()
         curses.curs_set(False)
         stdscr.nodelay(True)
+        stdscr.border()
 
         self.stdscr = stdscr
         self._window_size = stdscr.getmaxyx()
-
-        self.screen = MenuScreen(self)
 
     def _run(self, stdscr, args):
         self._setup(stdscr, args)
--- cobra/model.py
+++ cobra/model.py
@@ -1,9 +1,71 @@
-from random import randint
+from collections import deque
 
 
-class Game(object):
+class SnakeListener(object):
 
-    def __init__(self):
-        super(Game, self).__init__()
+    def body_updated(self, body):
+        pass
 
-        self.game_area = (80, 23)
+    def head_updated(self, head):
+        pass
+
+    def tail_updated(self, tail):
+        pass
+
+
+class Snake(object):
+
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+    DIRECTION_SIZE = 4
+
+    def __init__(self, body):
+        super(Snake, self).__init__()
+
+        self.body = deque(body)
+        self._direction = self.RIGHT
+
+        self._listener = None
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @direction.setter
+    def direction(self, value):
+        if self._can_turn(value):
+            self._direction = value
+
+    def _can_turn(self, direction):
+        return ((direction + self._direction) % 2) == 1
+
+    @property
+    def listener(self):
+        return self._listener
+
+    @listener.setter
+    def listener(self, value):
+        self._listener = value
+        self._listener.body_updated(self.body)
+
+    def update(self):
+        head = self._move(self.body[-1])
+        self.body.append(head)
+
+        self.listener.head_updated(head)
+        self.listener.tail_updated(self.body.popleft())
+
+    def _move(self, right):
+        head = [right[0], right[1]]
+        if self.direction == self.UP:
+            head[1] -= 1
+        elif self.direction == self.DOWN:
+            head[1] += 1
+        elif self.direction == self.LEFT:
+            head[0] -= 1
+        elif self.direction == self.RIGHT:
+            head[0] += 1
+
+        return head
