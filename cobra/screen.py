c302b7bac0957d8ee00e84452d3427bd24c3f124
Emerson MX <emersonmx@gmail.com>
2014-12-01 20:30:28 -0300

    Movendo código do renderer para GameScreen e criando o esquele da tela de espera.

--- cobra/screen.py
+++ cobra/screen.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 from cobra.model import WorldConfig, World, Snake
-from cobra.renderer import CursesRenderer
+from cobra.model import WorldListener, SnakeListener
 from cobra.gamepad import GamePad
 
 
@@ -40,21 +40,44 @@ class BaseScreen(Screen):
         return self.application.window_size
 
 
-class GameScreen(BaseScreen):
+class WaitGameScreen(BaseScreen):
+
+    def __init__(self, application, game_screen):
+        BaseScreen.__init__(self, application)
+
+        self.game_screen = game_screen
+
+    def show(self):
+        pass
+
+    def hide(self):
+        pass
+
+    def dispose(self):
+        pass
+
+    def update(self, delta):
+        pass
+
+
+class GameScreen(BaseScreen, SnakeListener, WorldListener):
 
     def __init__(self, application):
         BaseScreen.__init__(self, application)
 
         self.world = None
-        self.renderer = None
+        self.game_window = None
 
-        self._paused = False
+        self.snake_body = []
+        self.score = 0
+        self.food = ()
 
         self.create()
 
     def create(self):
         self._create_world()
-        self._create_renderer()
+        self._setup_listener()
+        self._create_game_window()
 
     def _create_world(self):
         config = self._create_world_config()
@@ -64,10 +87,12 @@ class GameScreen(BaseScreen):
     def _create_world_config(self):
         return WorldConfig()
 
-    def _create_renderer(self):
-        self.renderer = CursesRenderer(self.stdscr)
-        self.world.listener = self.renderer
-        self.world.snake.listener = self.renderer
+    def _setup_listener(self):
+        self.world.listener = self
+        self.world.snake.listener = self
+
+    def _create_game_window(self):
+        self.game_window = self.stdscr.derwin(1, 0)
 
     def show(self):
         self.stdscr.nodelay(True)
@@ -101,8 +126,7 @@ class GameScreen(BaseScreen):
         curses.napms(10)
 
         self.process_input()
-        if not self._paused:
-            self.world.update(delta)
+        self.world.update(delta)
 
         self.render()
 
@@ -110,7 +134,48 @@ class GameScreen(BaseScreen):
         self.gamepad.process_input()
 
     def render(self):
-        self.renderer.render()
+        self.stdscr.clear()
+        self._render_score()
+        self._render_food()
+        self._render_snake_body()
+        self._render_game_window_border()
+        self._update_screen()
+
+    def _render_score(self):
+        self.stdscr.addstr(0, 0, "Score: {}".format(self.score))
+
+    def _render_food(self):
+        x, y = self.food
+        self.game_window.addch(y + 1, x + 1, '*')
+
+    def _render_snake_body(self):
+        for x, y in self.snake_body:
+            self.game_window.addch(y + 1, x + 1, '#')
+
+    def _render_game_window_border(self):
+        self.game_window.border()
+
+    def _update_screen(self):
+        self.stdscr.noutrefresh()
+        self.game_window.noutrefresh()
+        curses.doupdate()
+
+    def snake_body_updated(self, body):
+        self.snake_body = body
+
+    def world_started(self, world):
+        x, y, width, height = world.bounds
+
+    def world_finished(self, world):
+        logger.info("Game Over")
+
+    def food_created(self, world):
+        self.food = world.food
+        logger.info("I see a yummy food at {} :B".format(self.food))
+
+    def score_updated(self, world):
+        self.score = world.score
+        logger.info("Score updated to {}".format(self.score))
 
 
 class GamePauseScreen(BaseScreen):
